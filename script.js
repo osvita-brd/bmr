552880f79da5062e75fc53d61cbdcf95a2e75a22
osvita <162641174+osvita-brd@users.noreply.github.com>
2026-04-30 11:28:32 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -93,6 +93,18 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
+function openModal(src) {
+  const modal = document.getElementById("imgModal");
+  const modalImg = document.getElementById("modalImg");
+
+  modal.style.display = "flex";
+  modalImg.src = src;
+}
+
+function closeModal() {
+  document.getElementById("imgModal").style.display = "none";
+}
+
 
 
 
